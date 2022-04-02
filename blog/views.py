26446af743a094363fc0dff378520b00a5c1aaae
Tarso Jabbes <tarsojabbes@gmail.com>
2022-04-02 16:58:13 -0300

    Requests and responses

--- blog/views.py
+++ blog/views.py
@@ -1,8 +1,9 @@
 from django.shortcuts import render
 from django.http import HttpRequest, HttpResponse, JsonResponse, request
 from django.contrib.auth.models import User, Group
-from rest_framework import viewsets
-from rest_framework import permissions
+from rest_framework.decorators import api_view, permission_classes
+from rest_framework.response import Response
+from rest_framework import status, permissions
 from .serializers import PostSerializer
 from .models import Post
 from rest_framework.parsers import JSONParser
@@ -11,39 +12,40 @@ from django.views.decorators.csrf import csrf_exempt
 from blog import serializers
 # Create your views here.
 
-@csrf_exempt
+@api_view(["GET", "POST"])
+@permission_classes((permissions.AllowAny,))
 def post_list(request):
     if request.method == 'GET':
         posts = Post.objects.all()
         serializer = PostSerializer(posts, many=True)
-        return JsonResponse(data=serializer.data, safe=False)
+        return Response(serializer.data)
+
     elif request.method == 'POST':
-        data = JSONParser().parse(request)
-        serializer = PostSerializer(data=data)
+        serializer = PostSerializer(data=request.data)
         if serializer.is_valid():
             serializer.save()
-            return JsonResponse(serializer.data, status=201)
-        return JsonResponse(serializer.errors, status=400)
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
-@csrf_exempt
+@api_view(["PUT", "DELETE", "GET"])
+@permission_classes((permissions.AllowAny,))
 def post_detail(request, pk):
     try:
         post = Post.objects.get(pk=pk)
-    except:
-        return HttpResponse(status=404)
+    except Post.DoesNotExist:
+        return Response(status=status.HTTP_404_NOT_FOUND)
     
     if request.method == 'GET':
         serializer = PostSerializer(post)
-        return JsonResponse(serializer.data)
+        return Response(serializer.data)
 
     elif request.method == "PUT":
-        data = JSONParser().parse(request)
-        serializer = PostSerializer(post, data=data)
+        serializer = PostSerializer(post, data=request.data)
         if serializer.is_valid():
             serializer.save()
-            return JsonResponse(serializer.data)
-        return JsonResponse(serializer.errors, status=400)
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
     
     elif request.method == "DELETE":
         post.delete()
-        return HttpResponse(status=204)
+        return Response(status=status.HTTP_204_NO_CONTENT)
