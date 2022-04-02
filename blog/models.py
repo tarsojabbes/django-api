c73bccc90f54f86b7cc83bcfca1a043b3025bbf1
Tarso Jabbes <tarsojabbes@gmail.com>
2022-04-02 16:34:32 -0300

    Initial commit

---
+++ blog/models.py
@@ -0,0 +1,16 @@
+from enum import auto
+from tkinter import CASCADE
+from django.db import models
+from django.contrib.auth.models import User
+
+# Create your models here.
+class Post(models.Model):
+    title = models.CharField(max_length=255)
+    slug = models.SlugField(max_length=255, unique=True)
+    body = models.TextField()
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self) -> str:
+        return self.title
+
