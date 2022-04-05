6e4d050ab3a11cf4f7e87a660860ff72c298c0a9
Tarso Jabbes <tarsojabbes@gmail.com>
2022-04-05 19:50:23 -0300

    Implements the authentication and authorization permissions for Post routes

--- blog/views.py
+++ blog/views.py
@@ -2,6 +2,7 @@ from django.contrib.auth.models import User
 from django.http import Http404
 from rest_framework.response import Response
 from rest_framework import status
+from blog.permissions import IsOwnerOrReadOnly
 from .serializers import PostSerializer, UserSerializer
 from .models import Post
 from rest_framework.views import APIView
@@ -11,10 +12,12 @@ from rest_framework import generics
 from rest_framework import generics
 
 class PostList(generics.ListCreateAPIView):
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
     queryset = Post.objects.all()
     serializer_class = PostSerializer
 
 class PostDetail(generics.RetrieveUpdateDestroyAPIView):
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly, IsOwnerOrReadOnly]
     queryset = Post.objects.all()
     serializer_class = PostSerializer
     
