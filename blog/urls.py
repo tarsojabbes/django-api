706770a59af645cf547115caf905fb830cf5300e
Tarso Jabbes <tarsojabbes@gmail.com>
2022-04-05 23:16:46 -0300

    Endpoints for attributes and Pagination

--- blog/urls.py
+++ blog/urls.py
@@ -3,8 +3,10 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("posts/", views.PostList.as_view()),
+    path("posts/", views.PostList.as_view(), name="post-list"),
     path("posts/<int:pk>/", views.PostDetail.as_view()),
-    path("users/", views.UserList.as_view()),
-    path("users/<int:pk>/", views.UserDetail.as_view())
+    path("users/", views.UserList.as_view(), name="user-list"),
+    path("users/<int:pk>/", views.UserDetail.as_view()),
+    path("", views.api_root),
+    path("posts/<int:pk>/body/", views.PostBody.as_view())
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,15 +1,13 @@
+from audioop import reverse
 from django.contrib.auth.models import User
-from django.http import Http404
 from rest_framework.response import Response
-from rest_framework import status
 from blog.permissions import IsOwnerOrReadOnly
 from .serializers import PostSerializer, UserSerializer
 from .models import Post
-from rest_framework.views import APIView
 from rest_framework import permissions
 from rest_framework import generics
-
-from rest_framework import generics
+from rest_framework.decorators import api_view
+from rest_framework.reverse import reverse
 
 class PostList(generics.ListCreateAPIView):
     permission_classes = [permissions.IsAuthenticatedOrReadOnly]
@@ -24,12 +22,24 @@ class PostDetail(generics.RetrieveUpdateDestroyAPIView):
     def perform_create(self, serializer):
         serializer.save(owner=self.request.user)
 
+class PostBody(generics.GenericAPIView):
+    queryset = Post.objects.all()
+    
+    def get(self, request, *args, **kwargs):
+        post = self.get_object()
+        return Response(post.body)
+
 class UserList(generics.ListAPIView):
     queryset = User.objects.all()
     serializer_class = UserSerializer
-
 
 class UserDetail(generics.RetrieveAPIView):
     queryset = User.objects.all()
     serializer_class = UserSerializer
 
+@api_view(["GET"])
+def api_root(request, format=None):
+    return Response({
+        "users": reverse("user-list", request=request, format=format),
+        "posts": reverse("post-list", request=request, format=format),
+    })
